5a46a5e486dba1076f20dfafc3eb803eceddc294
TriangleYJ <dpwns99@gmail.com>
2022-01-24 21:51:08 +0900

    Solve Day 11 - 46. permutations (less memory)

--- 46.permutations.cpp
+++ 46.permutations.cpp
@@ -8,25 +8,26 @@
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        int taken[7] = {0};
-        return permuteWithTaken(nums, taken);
+        vector<vector<int>> result;
+        vector<int> tmp(nums.size());
+        int cnt[7] = {0};
+        permuteCnt(nums, result, tmp, cnt, 0);
+
+        return result;
     }
 
-    vector<vector<int>> permuteWithTaken(vector<int>& nums, int taken[10]){
-        vector<vector<int>> res;
-        int loop_cnt = 0;
-        for(int i = 0; i < nums.size(); i++){
-            if(taken[i]) continue;
-            else loop_cnt++;
-            taken[i] = 1;
-            vector<vector<int>> p = permuteWithTaken(nums, taken);
-            for (int j = 0; j < p.size(); j++) p.at(j).push_back(nums.at(i));
-            res.insert(res.end(), p.begin(), p.end());
-            taken[i] = 0;
+    void permuteCnt(vector<int>& nums, vector<vector<int>>& result, vector<int>& tmp, int occ[7], int cnt){
+        if(cnt == nums.size()) result.push_back(tmp);
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if(occ[i]) continue;
+            occ[i] = 1;
+            tmp.at(cnt) = nums[i];
+            permuteCnt(nums, result, tmp, occ, cnt+1);
+            occ[i] = 0;
         }
-        if(loop_cnt == 0) return {{}};
-        return res;
     }
+
 };
 // @lc code=end
 
